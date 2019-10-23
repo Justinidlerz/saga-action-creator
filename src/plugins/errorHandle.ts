b3e31008a0a8b87fa2fc267b6384af96a4f36684
Justinidlerz <zqc.sunny@gmail.com>
2019-10-23 10:07:45 +0800

    Fix typings for errorHandle plugin

--- src/plugins/errorHandle.ts
+++ src/plugins/errorHandle.ts
@@ -1,8 +1,9 @@
+import { call } from 'redux-saga/effects';
 import AbstractPlugin from '../lib/AbstractPlugin';
 import { IDefinitionClassesRecord, IDefinitionsRecord } from '../typings/connection';
 import { IDefinitionObjectWithModule } from '../typings/creator';
 
-type IErrorHandle = (error: Error, details: IDefinitionObjectWithModule) => any;
+type IErrorHandle = (error: Error, details: IDefinitionObjectWithModule) => Generator<any, any, any>;
 
 const getErrorHandlePlugin = (errorHandle: IErrorHandle) => {
   return class ErrorHandle<
@@ -10,7 +11,7 @@ const getErrorHandlePlugin = (errorHandle: IErrorHandle) => {
     DC extends IDefinitionClassesRecord<DR>
   > extends AbstractPlugin<DR, DC> {
     public *errorHandle(error: Error, details: IDefinitionObjectWithModule): Generator<any, boolean, any> {
-      errorHandle(error, details);
+      yield call(errorHandle, error, details);
       return true;
     }
   };
